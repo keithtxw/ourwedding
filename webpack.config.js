0be9b740dfdb5ce63ff05f3c87f42922e186dab6
Keith Tan <keith_xw_tan@tech.gov.sg>
2018-10-01 23:17:37 +0800

    change main.js to bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const webpack = require("webpack");
 module.exports = {
    entry: "./src/index.js",
    output: {
-      filename: "main.js",
+      filename: "bundle.js",
       path: path.resolve(__dirname, 'dist')
    },
    module: {
