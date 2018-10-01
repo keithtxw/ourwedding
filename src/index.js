5258832f54335dae92412aa9227b3007cc63ca0f
Keith Tan <keith_xw_tan@tech.gov.sg>
2018-10-01 23:11:59 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import _ from "lodash";
+import "jquery";
+import "./styles/main.css";
+
+function init() {
+
+}
+
+var map;
+var marker;
+function initMap() {
+   var position = {
+      lat: 1.300193,
+      lng: 103.860676,
+   };
+
+   var mapOptions = {
+      center: position,
+      zoom: 17,
+   };
+
+   map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+   var markerOptions = {
+      position,
+      map: map,
+      animation: google.maps.Animation.DROP,
+      title: "ParkRoyal on Beach Road",
+   };
+
+   marker = new google.maps.Marker(markerOptions);
+}
+
+init();
+window.initMap = initMap;
