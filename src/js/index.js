e7cf8b5ca745ef747992033f09b61f8bf9906f18
Keith Tan <keith_xw_tan@tech.gov.sg>
2018-10-06 22:31:07 +0800

    Install url-loader

--- src/js/index.js
+++ src/js/index.js
@@ -4,86 +4,86 @@ import "../js/scroll.js";
 import "../styles/main.css";
 
 function init() {
-   initDaysLeftDisplay();
-   initGoogleMapScript();
+	initDaysLeftDisplay();
+	initGoogleMapScript();
 }
 
 var map;
 var marker;
 function initMap() {
-   var position = {
-      lat: 1.300193,
-      lng: 103.860676,
-   };
+	var position = {
+		lat: 1.300193,
+		lng: 103.860676,
+	};
 
-   var mapOptions = {
-      center: position,
-      zoom: 17,
-   };
+	var mapOptions = {
+		center: position,
+		zoom: 17,
+	};
 
-   map = new google.maps.Map(document.getElementById("map"), mapOptions);
+	map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-   var markerOptions = {
-      position,
-      map: map,
-      animation: google.maps.Animation.DROP,
-      title: "ParkRoyal on Beach Road",
-   };
+	var markerOptions = {
+		position,
+		map: map,
+		animation: google.maps.Animation.DROP,
+		title: "ParkRoyal on Beach Road",
+	};
 
-   marker = new google.maps.Marker(markerOptions);
+	marker = new google.maps.Marker(markerOptions);
 }
 
 function initGoogleMapScript() {
-   var script = document.createElement("script");
-   script.type = "text/javascript";
-   script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyDC-gf6dWwGkmYisLJiPPY_cSnI8pzsHSw&callback=initMap";
-   script.async = "async";
-   script.defer = "defer";
+	var script = document.createElement("script");
+	script.type = "text/javascript";
+	script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyDC-gf6dWwGkmYisLJiPPY_cSnI8pzsHSw&callback=initMap";
+	script.async = "async";
+	script.defer = "defer";
 
-   $("body").append(script);
+	$("body").append(script);
 }
 
 function initDaysLeftDisplay() {
-   var today = new Date();
-   var wedding = new Date("12/15/2018");
-   var timeDiff = Math.abs(today.getTime() - wedding.getTime());
-   var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+	var today = new Date();
+	var wedding = new Date("12/15/2018");
+	var timeDiff = Math.abs(today.getTime() - wedding.getTime());
+	var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
 
-   var daysLeftNumberText;
-   var daysLeftText;
-   var subText;
+	var daysLeftNumberText;
+	var daysLeftText;
+	var subText;
 
-   switch (true) {
-      case (diffDays > 1):
-         daysLeftNumberText = diffDays;
-         daysLeftText = "Days left to our big day.";
-         subText = "We are really excited to have you there!";
-         break;
-      case (diffDays === 1):
-         daysLeftNumberText = diffDays;
-         daysLeftText = "Day left to our big day.";
-         subText = "Are you excited as we are? See you there!";
-         break;
-      case (diffDays === 0):
-         daysLeftNumberText = undefined;
-         daysLeftText = "Today is the big day!";
-         subText = "See you there at 7:00PM.";
-         break;
-      default:
-         daysLeftNumberText = undefined;
-         daysLeftText = "Thank you for coming!";
-         subText = "It meant alot for us.";
-         break;
-   }
+	switch (true) {
+		case (diffDays > 1):
+			daysLeftNumberText = diffDays;
+			daysLeftText = "Days left to our big day.";
+			subText = "We are really excited to have you there!";
+			break;
+		case (diffDays === 1):
+			daysLeftNumberText = diffDays;
+			daysLeftText = "Day left to our big day.";
+			subText = "Are you excited as we are? See you there!";
+			break;
+		case (diffDays === 0):
+			daysLeftNumberText = undefined;
+			daysLeftText = "Today is the big day!";
+			subText = "See you there at 7:00PM.";
+			break;
+		default:
+			daysLeftNumberText = undefined;
+			daysLeftText = "Thank you for coming!";
+			subText = "It meant alot for us.";
+			break;
+	}
 
-   if (daysLeftNumberText) {
-      $("#days").text(daysLeftNumberText);
-   } else {
-      $("#days").hide();
-   }
+	if (daysLeftNumberText) {
+		$("#days").text(daysLeftNumberText);
+	} else {
+		$("#days").hide();
+	}
 
-   $("#bigday").text(daysLeftText);
-   $("#excited").text(subText);
+	$("#bigday").text(daysLeftText);
+	$("#excited").text(subText);
 }
 
 init();
